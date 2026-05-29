8be944eed0cf2dcc1a7b9b4cf7c614868c99a15e
Wasiewicz11 <filip.wasiewicz11@gmail.com>
2026-05-29 16:10:02 +0200

    Update TrainConfiguration model to use Optional fields with default values

--- Streamlit_titanic/models/trainConfig.py
+++ Streamlit_titanic/models/trainConfig.py
@@ -3,5 +3,5 @@ from typing import Optional
 
 
 class TrainConfiguration(BaseModel):
-    n_estimators: int = Field(..., description="Liczba drzew w lesie")
-    max_depth: Optional[int] = Field(..., description="Maksymalna głębokość drzewa (None = bez limitu)")
+    n_estimators: Optional[int] = Field(None, description="Liczba drzew w lesie (domyślnie 300)")
+    max_depth: Optional[int] = Field(None, description="Maksymalna głębokość drzewa (None = bez limitu)")
